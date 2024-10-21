085d1366a86a25e4da244a9602f015401f927cd4
Jhon Ospina <jhonospina2711@gmail.com>
2024-10-21 12:53:44 -0500

    Se realiza la validación del login y devuelve mensaje de error

--- projects/authapp/src/app/auth/pages/login-page/login-page.component.ts
+++ projects/authapp/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2'
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
+
 
 @Component({
   templateUrl: './login-page.component.html',
@@ -7,15 +12,24 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginPageComponent {
 
-    private fb = inject( FormBuilder );
+    private fb            = inject( FormBuilder );
+    private authService   = inject( AuthService);
+    private router        = inject( Router);
 
-
+    //! myForm permite validar la estructura del email y el password;
     public myForm: FormGroup = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
+      email: ['jospina@corredores.com', [Validators.required, Validators.email]],
+      password: ['123456', [Validators.required, Validators.minLength(6)]],
     });
 
     login() {
-      console.log(this.myForm.value);
+      const { email, password } = this.myForm.value;
+      this.authService.login( email, password )
+       .subscribe({
+        next: () => this.router.navigateByUrl('/dashboard'),
+        error: ( message ) => {
+          Swal.fire('Error', message, 'error')
+        }
+       });
     }
 }
--- projects/authapp/src/app/auth/services/auth.service.ts
+++ projects/authapp/src/app/auth/services/auth.service.ts
@@ -1,8 +1,8 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { enviroment } from '../../../environments/environments';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { AuthStatus, User } from '../interfaces';
+import { catchError, map, Observable, of, tap, throwError } from 'rxjs';
+import { AuthStatus, LoginResponse, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -15,9 +15,34 @@ export class AuthService {
   private _currentUser = signal<User|null>(null);
   private _authStatus = signal<AuthStatus>( AuthStatus.checking );
 
+  //! Esto permite exponer al mundo exterior los valores de mi signal, sin comprometer la información
+  public currentUser = computed( () => this._currentUser() );
+  public authStatus = computed( () => this._authStatus() );
+
   constructor() { }
 
   login( email: string, password: string ): Observable<boolean>{
-    return of(true);
+
+    const url = `${ this.baseUrl }/auth/login`;
+    const body = { email: email, password };
+
+    return this.http.post<LoginResponse>( url, body )
+    .pipe(
+      tap( ({ user, token }) => {
+        this._currentUser.set( user );
+        this._authStatus.set( AuthStatus.authenticated);
+        //! Almaceno el token en el lcalstorage
+        localStorage.setItem('token', token );
+        console.log({user, token});
+      }),
+      map( () => true ),
+
+      //Todo: Errores
+      //! Aqui atrapamos el error de login
+      catchError( err => throwError( () => err.error.message )
+
+      )
+    );
+
   }
 }
